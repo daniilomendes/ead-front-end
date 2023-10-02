53e02bd3a8175edbf5b6ef9cafd69c2aea468b77
Danilo Mendes <danilo.mirandha@gmail.com>
2023-10-02 18:28:12 -0300

    Configuração do projeto, das pastas e arquivos, instalação das libs

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import styles from "../styles/homeNoAuth.module.scss"
+
+
+const HomeNotAuth = () => {
+  return (
+		<>
+			<Head>
+                <title>HOME</title>
+            </Head>
+			<main></main>
+		</>
+    )
+};
+
+export default HomeNotAuth;
