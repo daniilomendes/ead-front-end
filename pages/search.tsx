239a3edadcfd92bae28975267ce68289cb7dd908
Danilo Mendes <danilo.mirandha@gmail.com>
2023-10-05 14:04:24 -0300

    Backend do Search

---
+++ pages/search.tsx
@@ -0,0 +1,42 @@
+import styles from "../styles/search.module.scss"
+import Head from "next/head"
+import HeaderAuth from "@/components/common/headerAuth"
+import { useRouter } from "next/router"
+import { useState, useEffect } from "react"
+import courseService, { CourseType } from "@/services/courseService"
+
+const Search = () => {
+
+    const router = useRouter()
+    const searchName: any = router.query.name
+    const [searchResult, setSearchResult] = useState<CourseType[]>([])
+
+    const seachCourses = async function () {
+    const res = await courseService.getSearch(searchName)
+        setSearchResult(res.data.courses)
+
+    }
+
+    useEffect(() => {
+        seachCourses()
+    }, [searchName])
+
+    return (
+        <>
+            <Head>
+                <title>Onebitflix - {searchName}</title>
+                <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+            </Head>
+            <main>
+                <HeaderAuth/>
+                {searchResult?.map((course) => (
+                    <div key={course.id}>
+                        <p>{course.name}</p>
+                    </div>
+                ))}
+            </main>
+        </>
+    )
+}
+
+export default Search
--- src/components/common/headerAuth/index.tsx
+++ src/components/common/headerAuth/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 import { Form, FormGroup, Label, Container, Button, Input } from 'reactstrap'
 import styles from "./styles.module.scss"
 import Modal from "react-modal"
-import { useEffect, useState } from "react"
+import { ChangeEvent, FormEvent, useEffect, useState } from "react"
 import { useRouter } from "next/router"
 import profileService from "@/services/profileService"
 
@@ -13,6 +13,19 @@ const HeaderAuth = () => {
     const router = useRouter()
     const [modalOpen, setModalOpen] = useState(false)
     const [initials, setInitials] = useState("")
+    const [searchName, setSearchName] = useState("")
+    
+    const handleSearch = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+        router.push(`search?name=${searchName}`)
+        setSearchName("")
+    }
+
+    const handleSearchClick = () => {
+        router.push(`search?name=${searchName}`)
+        setSearchName("")
+    }
 
     useEffect(() => {
         profileService.fetchCurrent().then((user) => {
@@ -43,10 +56,11 @@ const HeaderAuth = () => {
                     <img src="/logoOnebitflix.svg" alt="logoOnebitflix" className={styles.imgLogoNav}/>
                 </Link>
                 <div className="d-flex align-items-center">
-                    <Form>
-                        <Input name="search" type="search" placeholder="Pesquisar" className={styles.input}/>
+                    <Form onSubmit={handleSearch}>
+                        <Input name="search" type="search" placeholder="Pesquisar" className={styles.input} 
+                        value={searchName} onChange={(event: ChangeEvent<HTMLInputElement>) => {setSearchName(event.currentTarget.value.toLowerCase())}}/>
                     </Form>
-                    <img src="/homeAuth/iconSearch.svg" alt="lupaHeader" className={styles.searchImg}/>
+                    <img src="/homeAuth/iconSearch.svg" alt="lupaHeader" className={styles.searchImg} onClick={handleSearchClick}/>
                     <p className={styles.userProfile} onClick={handleOpenModal}>{initials}</p>
                 </div>
                 <Modal isOpen={modalOpen} onRequestClose={handleCloseModal} shouldCloseOnEsc={true} className={styles.modal} overlayClassName={styles.overlayModal}>
